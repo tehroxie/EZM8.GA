0cf3454f626abde04973c9471b79d63351d982fd
roxie <tehroxie27@gmail.com>
2016-04-30 14:31:11 +0100

    Eye Icon

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,3 +1,34 @@
-$.(function(){
+$(function(){
+
   $().addClass("loaded");
+
+  $('.signin-form .input-password .show-toggle').click(function(){
+
+    $(this).toggleClass("fa-eye-slash",$(this).hasClass("fa-eye"));
+    $(this).toggleClass("fa-eye",!$(this).hasClass("fa-eye-slash"));
+
+    if($('.signin-form .input-password .show-toggle').hasClass("fa-eye-slash")) {
+      $('.signin-form .input-password .input-field').clone().attr('type','text').insertAfter('.signin-form .input-password .input-field').prev().remove();
+
+    } else {
+      $('.signin-form .input-password .input-field').clone().attr('type','password').insertAfter('.signin-form .input-password .input-field').prev().remove();
+
+    }
+
+  });
+
+  $('.signin-form .input-password .input-field').blur(function(){
+      if($(this).val() === "" ) {
+        $('.signin-form .input-password').removeClass("has-value");
+      }
+  });
+
 });
+
+function toggle() {
+  if($('.input-password').hasClass("has-value")) {
+    $('.signin-form .input-password .show-toggle').show();
+  } else {
+    $('.signin-form .input-password .show-toggle').hide();
+  }
+}
